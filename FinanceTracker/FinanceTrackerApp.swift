04d194b272efa51867d5a051738793206997256e
Amanda Li <a383li@uwaterloo.ca>
2023-05-05 16:26:35 -0400

    Initial Commit

---
+++ FinanceTracker/FinanceTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceTrackerApp.swift
+//  FinanceTracker
+//
+//  Created by Amanda Li on 2023-05-05.
+//
+
+import SwiftUI
+
+@main
+struct FinanceTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
