9c592114f9ff34a4dcaa824b086dfd408d6e87a5
Amanda Li <a383li@uwaterloo.ca>
2023-05-06 21:22:21 -0400

    hsiang

--- FinanceTracker/ContentView.swift
+++ FinanceTracker/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world! lol")
+            Text("Hello, world! jdawiojdsi")
         }
         .padding()
     }
